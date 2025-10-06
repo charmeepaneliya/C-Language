9a17dc39b8d5324771dfc240bfb32baff8975612
charmeepaneliya <cpjstudy28@gmail.com>
2025-10-06 15:47:44 +0530

    data type & variable

---
+++ 03-data type & variable/datatype-variable.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int num = 10;
+
+    printf("%d", num);
+
+    char *str ="abcd";
+
+    printf("\nstring:%s", str);
+
+    float pi = 3.14;
+
+    printf("\ndecimal value:%", pi);
+
+    return 0;
+}
