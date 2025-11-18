7f8fc8f8c2d7361ac18d7b174c1fd20175310254
Charmee Paneliya <cpjstudy28@gmail.com>
2025-11-18 12:13:09 +0530

    break , continue, go to

---
+++ ch-5_c_control_statements/goto.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+   
+        int num=10;
+
+        if(num<5){
+            goto small;
+        }else{
+            goto big;
+        }
+
+        small:
+        printf("number is smaller then 5");
+
+        big:
+        printf("number is greater than 5");
+   
+        return 0;
+}
